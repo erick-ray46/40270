2d8415951d64d9277954681db7e001fa5a2b1a4f
ercik-ray46 <erick.iy.gutrz46@gmail.com>
2026-05-20 16:55:30 -0600

    produccion js

---
+++ javascript/modulos/dist/xyz.js
@@ -0,0 +1,2 @@
+(()=>{function r(o){return console.log("y: "+o),"fin y"}function n(o){return console.log("x: "+o),"fin x"}function l(o){return"fin xx"}function e(o){return console.log("z: "+o),"fin z"}console.log(n("abc"));console.log(l("def"));console.log(n("abc"));console.log(l("def"));console.log(r("ghi"));console.log(e("jkl"));})();
+//# sourceMappingURL=xyz.js.map
